d5ef49425c126e405b8825c54cd2945278167763
Steven Hancock <steveh@rapidintake.com>
2016-05-05 17:22:33 -0600

    Added coffee script files.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
+var coffeeSources = ['components/coffee/tagline.coffee'];
 
+gulp.task('coffee', function() {
+    gulp.src(coffeeSources[0]) //Location of original source files you are going to process.
+        .pipe(coffee({ bare: true }) //pipe the file to the coffee object with options
+            .on('error', gutil.log)) //log any errors so that execution doesn't stop
+        .pipe(gulp.dest('components/scripts')); //location of output file.
 });
