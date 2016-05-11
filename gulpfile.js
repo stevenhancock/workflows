3b085c601813609b21e4fc7773cd5de242927e1b
Steven Hancock <steveh@rapidintake.com>
2016-05-11 17:41:23 -0600

    Set up Sass and compass processing

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,9 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
-    concat = require('gulp-concat');
+    concat = require('gulp-concat'),
+    compass = require('gulp-compass'),
+    browserify = require('gulp-browserify');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsSources = [
@@ -10,6 +12,7 @@ var jsSources = [
     'components/scripts/tagline.js',
     'components/scripts/template.js',
 ];
+var sassSources = ['components/sass/style.scss'];//style.scss uses sass import to import the other sass files.
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources) //Location of original source files you are going to process.
@@ -21,5 +24,17 @@ gulp.task('coffee', function() {
 gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
+        .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'));
+});
+
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        }))
+        .on('error', gutil.log)
+        .pipe(gulp.dest('builds/development/css'));
 });
