04acf2c5a73ec8f0e5abe4e708e75c0c7f706bc2
Steven Hancock <steveh@rapidintake.com>
2016-05-12 17:49:31 -0600

    Added gulp watch task.

--- gulpfile.js
+++ gulpfile.js
@@ -38,3 +38,11 @@ gulp.task('compass', function() {
         .on('error', gutil.log)
         .pipe(gulp.dest('builds/development/css'));
 });
+
+gulp.task('watch', function() {
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(jsSources, ['js']);
+    gulp.watch('components/sass/*.scss', ['compass']);
+});
+
+gulp.task('all', ['coffee', 'js', 'compass']); //Executes all three.
